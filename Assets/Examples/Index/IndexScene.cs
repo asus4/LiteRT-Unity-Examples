74b22b38aeba552b60762fdff0c6fc4ccb4f7662
Koki Ibukuro <koki.ibukuro@gmail.com>
2024-10-17 12:50:25 +0200

    Initial commit

---
+++ Assets/Examples/Index/IndexScene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class IndexScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# LiteRT Unity Examples
